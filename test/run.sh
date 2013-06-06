eccf777df2fb4b89b2502c9c65c4d2d0f88f6393
Elad Ben-Israel <elad.benisrael@gmail.com>
2013-06-05 17:52:22 -0700

    Add -i for interactive mode

--- test/run.sh
+++ test/run.sh
@@ -3,3 +3,4 @@
 ../gameoflife < input2.txt | diff -w output2.txt -
 ../gameoflife < input3.txt | diff -w output3.txt -
 ../gameoflife < input4.txt | diff -w output4.txt -
+../gameoflife < input5.txt | diff -w output5.txt -
