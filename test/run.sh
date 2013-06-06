5d7834361b0dae079ca5b8282c8df730823b1e79
Elad Ben-Israel <elad.benisrael@gmail.com>
2013-06-05 17:35:11 -0700

    Some orgs

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-node ../gameoflife.js < input1.txt | diff -w output1.txt -
-node ../gameoflife.js < input2.txt | diff -w output2.txt -
-node ../gameoflife.js < input3.txt | diff -w output3.txt -
-node ../gameoflife.js < input4.txt | diff -w output4.txt -
+../gameoflife < input1.txt | diff -w output1.txt -
+../gameoflife < input2.txt | diff -w output2.txt -
+../gameoflife < input3.txt | diff -w output3.txt -
+../gameoflife < input4.txt | diff -w output4.txt -
